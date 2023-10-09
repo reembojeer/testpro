0954c1861f89ab1b7241e84d126680004969cf5c
reembojeer <reembojeer@gmail.com>
2023-10-09 20:51:41 +0300

    Initial Commit

---
+++ projectt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projectt
+//
+//  Created by RIM ABDULLAH BUJEIR on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projectt/projecttApp.swift
@@ -0,0 +1,17 @@
+//
+//  projecttApp.swift
+//  projectt
+//
+//  Created by RIM ABDULLAH BUJEIR on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct projecttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
